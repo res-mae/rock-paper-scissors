57c97589868c830a4975d3ce7b18c17f27bad5e1
mae <110599787+mar19999@users.noreply.github.com>
2025-06-09 06:48:31 +0800

    create initial game

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # rock-paper-scissors
+
+This is a simple **Rock Paper Scissors** game developed as part of **The Odin Project's** Foundations Course.
+
+## Project Overview
+
+This project serves as an introduction to the basic **JavaScript** fundamentals by creating a simple **Rock Paper Scissors** game. It focuses on teaching core programming concepts such as functions, conditionals, loops, and handling user input.
+
+## Notes
+
+* The game is played entirely through the **browser console**.
+* Users are assumed to enter valid choices (rock, paper, or scissors).
+* As the course progresses, future updates may include a graphical user interface (GUI) and additional features to improve interactivity and user experience.
+
+## Instructions
+
+For detailed instructions on the project, visit the official [The Odin Project: Rock Paper Scissors](https://www.theodinproject.com/lessons/foundations-rock-paper-scissors).
+
+## Technologies
+ * **HTML**
+ * **JavaScript**
---
+++ script.js
@@ -0,0 +1,79 @@
+let humanScore = 0;
+let computerScore = 0;
+
+playGame();
+
+function getComputerChoice() {
+    const randomChoice = ["rock", "paper", "scissors"]
+    return randomChoice[Math.floor(Math.random() * 3)];
+}
+
+function getHumanChoice() {
+    const sign = prompt("rock, paper, or scissors?");
+    return sign.toLowerCase();
+}
+
+function playRound(humanChoice, computerChoice) {
+    if (humanChoice == computerChoice) {
+        console.log("Draw! Both players chose " + humanChoice);
+        console.log("Retry Round");
+
+        let humanSelection = getHumanChoice();
+        let computerSelection = getComputerChoice();
+
+        playRound(humanSelection, computerSelection);
+
+    }
+
+    if (humanChoice == "rock") {
+        if (computerChoice == "paper") {
+            console.log("You lose! Paper beats Rock");
+            computerScore++;
+        } else if (computerChoice == "scissors") {
+            console.log("You win! Rock beats Scissors");
+            humanScore++;
+        }
+    } else if (humanChoice == "paper") {
+        if (computerChoice == "rock") {
+            console.log("You win! Paper beats Rock");
+            humanScore++;
+        } else if (computerChoice == "scissors") {
+            console.log("You lose! Scissors beat Paper");
+            computerScore++;
+        }
+    } else if (humanChoice == "scissors") {
+        if (computerChoice == "rock") {
+            console.log("You lose! Rock beats Scissors");
+            computerScore++;
+        } else if (computerChoice == "paper") {
+            console.log("You win! Scissors beat Paper");
+            humanScore++;
+        }
+    }
+}
+
+function playGame() {
+    let retryRound = false;
+
+    for (let i = 1; i <= 5; i++) {
+        console.log("\nRound " + i);
+
+        let humanSelection = getHumanChoice();
+        let computerSelection = getComputerChoice();
+        
+        playRound(humanSelection, computerSelection);
+
+        console.log("Your score: " + humanScore);
+        console.log("Computer score: " + computerScore);
+
+    }
+
+    if (humanScore > computerScore) {
+        console.log("\nYou win!");
+    } else if (computerScore > humanScore) {
+        console.log("\nYou lose!");
+    }
+
+    return console.log("Final score: " + humanScore + "-" + computerScore);
+    
+}
